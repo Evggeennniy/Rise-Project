9b322763ca5c1d9dc14e607012d63270124194ce
FiX <myiphone128@mail.ru>
2023-08-31 08:03:01 +0300

    urls

--- app/services/urls.py
+++ app/services/urls.py
@@ -7,6 +7,8 @@ from services import views as services_views
 Посилання програми.
 """
 urlpatterns = [
-    path('services_categories', services_views.ServiceCategoryView.as_view(), name='services_category'),
+    path('services_category/', services_views.ServiceCategoryView.as_view(), name='services_category'),
+    path('services_category/<int:pk>', services_views.ServiceCategoryView.as_view(), name='services_category'),
     path('create_order/<int:pk>', services_views.ServiceOrderCreate.as_view(), name='create_service_order'),
+    # path('order_created', services_views.OrderCreatedSuccessfulView.as_view(), name='order_create')
 ]
