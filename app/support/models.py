a9af626cc1aae8e90b26759cbea43cbd7f637e50
FiX <myiphone128@mail.ru>
2023-09-07 16:17:13 +0300

    some charfields replaced to textfields

--- app/support/models.py
+++ app/support/models.py
@@ -7,12 +7,12 @@ class SupportMail(models.Model):
     """
     client = models.ForeignKey(verbose_name='Користувач', to='accounts.User', on_delete=models.CASCADE, )
     thema = models.CharField(verbose_name='Тема', max_length=32)
-    question = models.CharField(verbose_name='Опис', max_length=512)
-    notes = models.CharField(verbose_name='Примітки', max_length=64)
-    report = models.ImageField(verbose_name='Cкріншот', upload_to='reports',blank=True)
+    question = models.TextField(verbose_name='Опис', max_length=512)
+    notes = models.CharField(verbose_name='Примітки', max_length=64, blank=True)
+    report = models.ImageField(verbose_name='Cкріншот', upload_to='reports', blank=True)
     data = models.DateTimeField(verbose_name='Дата', auto_now_add=True, blank=True,)
 
-    answer = models.CharField(verbose_name='Відповідь', max_length=128, blank=True)
+    answer = models.TextField(verbose_name='Відповідь', max_length=256, blank=True)
 
     def __str__(self) -> str:
         return f'Тема {self.thema}, Опис {self.question}, Дата {self.data}'
