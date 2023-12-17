0951a9bc8c34248630ed7f0cc8c4b09cbf4b50b6
FiX <myiphone128@mail.ru>
2023-12-17 12:32:16 +0200

    others

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ GLOBALSMM_API_URL = 'https://global-smm.com/api/v2/'
 GLOBALSMM_API_KEY = 'b3e9a678aca8c3657dd23ef40572368e'
 
 client = GlobalApiClient(GLOBALSMM_API_URL, GLOBALSMM_API_KEY)
-status = client.get_status(308410531)
+status = client.create_order(105, 67, 'https://www.instagram.com/p/Cyyp5vZKcSU/?igshid=MzRlODBiNWFlZA==')
 print(status)
